247320651f7ba7a497a6d5f202bff9621658d28a
Sugihara Masaki <sugihara0227@gmail.com>
2019-08-23 10:08:45 +0700

    Add Create Action To Posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,20 @@
 class PostsController < ApplicationController
   def new
-    @post = Post.new
+    @post = current_user.posts.build
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
   end
 end
